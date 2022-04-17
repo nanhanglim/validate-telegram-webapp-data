eab75be3d059846fd28c4ba3c7578d9efdd45cb1
Nan Hang Lim <190704d@mymail.nyp.edu.sg>
2022-04-18 01:11:10 +0800

    fix bug, add readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# validate-telegram-webapp-data
- 
+# Validate Telegram WebApp Data
+
+Library to [validate Telegram WebApp Data](https://core.telegram.org/bots/webapps#validating-data-received-via-the-web-app)
+
+---
+# Installation
+```
+npm i --save @nanhanglim/validate-telegram-webapp-data
+```
+
+# Usage
+```
+// import library
+const ValidateTelegramWebAppData = require('@nanhanglim/validate-telegram-webapp-data');
+
+// initialise instance
+const VTWAD = new ValidateTelegramWebAppData('BOT_TOKEN_HERE');
+
+...
+
+// check if data is valid/not expired
+const dataIsValid = VTWAD.ValidateData(initData, 3600); // 1 hour
+```
+
+You may also set `secondsToExpire` to 0 to ignore expiry check
+
+```
+const dataIsValid = VTWAD.ValidateData(initData, 0); // never expires
+```
--- index.js
+++ index.js
@@ -3,24 +3,32 @@ const crypto = require("crypto");
 class ValidateTelegramWebAppData {
     /**
      * Creates ValidateTelegramWebAppData instance.
-     * @param {string} telegram_bot_token 
+     * @constructor
+     * @param {string} telegram_bot_token - your Telegram bot's token
      */
     constructor(telegram_bot_token) {
         this.telegram_bot_token = telegram_bot_token;
     }
 
     /**
-     * Returns true if data is valid; false if data is invalid.
-     * @param {object} data 
+     * Returns true if data is valid; false if data is invalid/has expired.
+     * @param {object} data - object of initData received from Telegram
+     * @param {number} secondsToExpire - number of seconds to expire from auth_date; enter 0 to ignore expiry check
      */
-    ValidateData({hash, ...data}) {
+    ValidateData({hash, ...data}, secondsToExpire) {
         const data_check_string = Object.keys(data)
         .sort()
         .map(key => `${key}=${data[key]}`)
         .join('\n');
-        const secret_key = crypto.createHmac("sha256", "WebAppData").update(this.telegram_bot_token).digest('hex');
+        const secret_key = crypto.createHmac('sha256', 'WebAppData').update(this.telegram_bot_token).digest();
         const hmac = crypto.createHmac('sha256', secret_key).update(data_check_string).digest('hex');
-        return hmac === hash ? true : false;
+        const currentTime = parseInt(Date.now() / 1000);
+        const currentAndAuthTimeDiff = currentTime - parseInt(data['auth_date']);
+        if (secondsToExpire == 0) {
+            return hmac === hash ? true : false;
+        } else {
+            return currentAndAuthTimeDiff < secondsToExpire && hmac == hash ? true : false;
+        }
     }
 }
 
