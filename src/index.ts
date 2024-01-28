3cfca8bde55b66344645b6c2a8473bb2cdf3f87c
Nan Hang Lim <58681238+nanhangl@users.noreply.github.com>
2024-01-28 20:07:02 +0800

    fix index signature issue

--- src/index.ts
+++ src/index.ts
@@ -54,35 +54,36 @@ interface ValidateDataResponse {
 }
 
 interface WebAppUser {
-    id: number,
-    is_bot?: boolean,
-    first_name: string,
-    last_name?: string,
-    username?: string,
-    language_code?: string,
-    is_premium?: true,
-    added_to_attachment_menu?: true,
-    allows_write_to_pm?: true,
-    photo_url?: string
+    id: number;
+    is_bot?: boolean;
+    first_name: string;
+    last_name?: string;
+    username?: string;
+    language_code?: string;
+    is_premium?: true;
+    added_to_attachment_menu?: true;
+    allows_write_to_pm?: true;
+    photo_url?: string;
 };
 
 interface WebAppChat {
-    id: number,
-    type: string,
-    title: string,
-    username?: string,
-    photo_url?: string
+    id: number;
+    type: string;
+    title: string;
+    username?: string;
+    photo_url?: string;
 };
 
 interface WebAppInitData {
-    query_id?: string,
-    user?: WebAppUser,
-    receiver?: WebAppUser,
-    chat?: WebAppChat,
-    chat_type?: string,
-    chat_instance?: string,
-    start_param?: string,
-    can_send_after?: number,
-    auth_date?: number | undefined,
-    hash?: string | undefined
+    [key: string]: any;
+    query_id?: string;
+    user?: WebAppUser;
+    receiver?: WebAppUser;
+    chat?: WebAppChat;
+    chat_type?: string;
+    chat_instance?: string;
+    start_param?: string;
+    can_send_after?: number;
+    auth_date?: number | undefined;
+    hash?: string | undefined;
 };
